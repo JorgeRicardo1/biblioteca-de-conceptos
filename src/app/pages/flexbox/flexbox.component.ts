619c10613a4a2e241ce3c2e7e160f6d18a1c692e
jorgeRicardo1 <jorge.ricardo@utp.edu.co>
2024-08-23 11:35:01 -0500

    build: se creo la opcion para propiedaded container

--- src/app/pages/flexbox/flexbox.component.ts
+++ src/app/pages/flexbox/flexbox.component.ts
@@ -1,35 +1,68 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectChange, MatSelectModule} from '@angular/material/select';
+import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule,FormsModule  } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 
 @Component({
   selector: 'app-flexbox',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatInputModule, ReactiveFormsModule],
+  imports: [CommonModule, MatFormFieldModule,FormsModule, MatSelectModule,MatRadioModule,
+    MatInputModule, ReactiveFormsModule, MatCheckboxModule, MatExpansionModule],
   templateUrl: './flexbox.component.html',
   styleUrl: './flexbox.component.css'
 })
 export class FlexboxComponent {
   flexDirectionOption = 'row';
   justifyContentOption = 'flex-start';
-  alignItemsOption = 'flex-start';
-  flexWrapOption = 'wrap';
+  alignItemsOption = 'stretch';
+  flexWrapOption = 'nowrap';
+  alignContentOption = 'stretch';
   numeroElementos =  new FormControl(1);
+  widthElementos = new FormControl(100);
+  heightElementos = new FormControl(100);
 
   elementosArray = Array(this.numeroElementos.value).fill(0);
 
+  private subscriptions = new Subscription();
+
   ngOnInit() {
-    this.numeroElementos.valueChanges.subscribe(value => {
-      this.elementosArray = Array(value).fill(0).map( (x, y) => y );
-    });
+
   }
 
-  onFlexDirectionChange(event: MatSelectChange) {
-    console.log('flexDirectionOption changed:', event.value);
-    // Puedes agregar más lógica aquí si es necesario
+  constructor(){
+    this.subscriptions.add(
+      this.numeroElementos.valueChanges.subscribe(value => {
+        this.elementosArray = Array(value).fill(0).map( (x, y) => y );
+      })
+    );
+
+    this.subscriptions.add(
+      this.widthElementos.valueChanges.subscribe(value => {
+        console.log('value');
+      })
+    );
+
+    this.subscriptions.add(
+      this.heightElementos.valueChanges.subscribe(value => {
+        console.log('value');
+      })
+    );
+
+  }
+
+  onOptionChange(newValue: string, optionName: string): void {
+    console.log(`${optionName} changed to: ${newValue}`);
+  }
+
+  ngOnDestroy(){
+    this.subscriptions.unsubscribe();
   }
 }

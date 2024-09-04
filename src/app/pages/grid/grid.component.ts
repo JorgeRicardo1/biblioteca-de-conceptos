d5ad66576f350dd0eedec4ad9b69c8c16d837171
jorgeRicardo1 <jorge.ricardo@utp.edu.co>
2024-09-04 17:32:24 -0500

    build: maquetacion de grid -parameters section y excibicion

--- src/app/pages/grid/grid.component.ts
+++ src/app/pages/grid/grid.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatExpansionModule],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css'
 })

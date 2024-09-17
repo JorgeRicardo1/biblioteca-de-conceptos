3be96ea2053309c9d7297f35c564809c7f883b67
jorgeRicardo1 <jorge.ricardo@utp.edu.co>
2024-09-17 00:20:14 -0500

    build: se creo el comportamiento drag and drop

--- src/app/pages/grid/grid.component.ts
+++ src/app/pages/grid/grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { Component, ElementRef, QueryList, signal, ViewChildren } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -8,12 +8,13 @@ import { CommonModule } from '@angular/common';
 import { ControlGrid } from './interfaces/controlGrid';
 import { MatIconModule } from '@angular/material/icon';
 import { itemGrid } from './interfaces/itemGrid';
+import { CdkDragEnd, CdkDragMove, CdkDragStart, DragDropModule, DragRef, Point } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-grid',
   standalone: true,
   imports: [MatFormFieldModule, MatInputModule, MatExpansionModule, ReactiveFormsModule, FormsModule,
-    CommonModule, MatIconModule],
+    CommonModule, MatIconModule, DragDropModule],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css'
 })
@@ -31,6 +32,9 @@ export class GridComponent {
   columnGap = new FormControl(0);
 
   subscriptions = new Subscription();
+
+  previusPosition = { x: 0, y:0};
+  dragPosition = signal({ x: 0, y: 0 });
 
   constructor() {
     this.subscriptions.add(
@@ -64,6 +68,8 @@ export class GridComponent {
         this.createControlGrid();
       })
     )
+
+    this.createControlGrid();
   }
 
   ngAfterViewInit() {
@@ -71,12 +77,7 @@ export class GridComponent {
       const computedStyle = window.getComputedStyle(element.nativeElement);
       const gridColumnStart = computedStyle.getPropertyValue('grid-column-start');
       const gridRowStart = computedStyle.getPropertyValue('grid-row-start');
-      console.log(`Elemento ${index + 1}: Columna: ${gridColumnStart}, Fila: ${gridRowStart}`);
     });
-  }
-
-  ngOnInit() {
-    this.createControlGrid();
   }
 
   getGridStyles() {
@@ -88,26 +89,71 @@ export class GridComponent {
     }
   }
 
-  // obtenerPosicionGrid(index: number) {
-  //   const element = this.elements.toArray()[index];
-  //   if (element) {
-  //     const computedStyle = window.getComputedStyle(element.nativeElement);
-  //     const gridColumnStart = computedStyle.getPropertyValue('grid-column-start');
-  //     const gridRowStart = computedStyle.getPropertyValue('grid-row-start');
-  //     console.log(`Posición del elemento: Columna: ${gridColumnStart}, Fila: ${gridRowStart}`);
-  //   }
-  // }
+  onDragEnded(event: CdkDragEnd, item: itemGrid) {
+    const gridElement = document.querySelector('.grid-interactive') as HTMLElement;
+
+    const gridRect = gridElement.getBoundingClientRect();
+    const elementRect = event.source.element.nativeElement.getBoundingClientRect();
+
+
+
+    if (!this.columns.value || !this.rows.value) {
+      return;
+    }
+
+    // Tamaño de cada celda en la cuadrícula
+    const cellWidth = gridRect.width / this.columns.value;
+    const cellHeight = gridRect.height / this.rows.value;
+
+    // Obtener la posición donde terminó el drag
+
+    const newColumn = Math.round((elementRect.left - gridRect.left) / cellWidth) + 1;
+    const newRow = Math.round((elementRect.top - gridRect.top) / cellHeight) + 1;
+
+    // Asegurarse de que el elemento no se salga del grid
+    item.column = Math.max(1, Math.min(newColumn, this.columns.value));
+    item.row = Math.max(1, Math.min(newRow, this.rows.value));
+
+    // Forzar actualización del grid
+
+    event.source.element.nativeElement.style.transform = '';
+
+    this.previusPosition.x = elementRect.left;
+    this.previusPosition.y = elementRect.top;
+
+    console.log('previusPosition x',  this.previusPosition.x  );
+    console.log('previusPosition y', this.previusPosition.y);
+
+    console.log(this.addedElementsList)
+  }
+
+
+  onDragStarted(event: CdkDragStart, item: itemGrid) {
+    console.log("started")
+    this.dragPosition.update(current => {
+      const newPosition = {
+        x: 0,
+        y: 0
+      };
+      console.log('Actualizando a:', newPosition);
+      return newPosition;
+    });
+  }
+
+  cdkDragMoved(event: CdkDragMove, item: itemGrid) {
+
+  }
+
 
   createNewElement(index: number) {
     const element = this.controlGridList.find(el => el.id === index);
     if (element) {
       this.addedElementsList.push({
-        color:  `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 80)} , ${Math.floor(Math.random() * 256)}, 0.5)`,
-        column : element.column,
-        row : element.row,
-        id : this.addedElementsList.length + 1
+        color: `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 80)} , ${Math.floor(Math.random() * 256)}, 0.5)`,
+        column: element.column,
+        row: element.row,
+        id: this.addedElementsList.length + 1
       });
-      console.log(this.addedElementsList);
     }
   }
 
@@ -124,7 +170,7 @@ export class GridComponent {
 
         columIndex += 1;
 
-        if(columIndex > this.columns.value){
+        if (columIndex > this.columns.value) {
           columIndex = 1;
           rowIndex += 1;
         }
